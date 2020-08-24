bccca15b63ba4ab0d2c24cd3f52a0a92aa490ce7
wazman08 <70171439+wazman08@users.noreply.github.com>
2020-08-24 14:50:40 -0500

    Update app.js

--- site/app.js
+++ site/app.js
@@ -52,8 +52,8 @@ app.get('/info', function(req, res) {
   if (CURRENTMODE == MODE.TEST) {
     patientdata = {
       "personal": {
-        "name": "Ralph DAlmeida",
-        "age": 38,
+        "name": "WAZMAN",
+        "age": 55,
         "gender": "male",
         "street": "34 Main Street",
         "city": "Toronto",
